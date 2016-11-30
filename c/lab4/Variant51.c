99aebdbadd1eb58f450913d263dc7e433563c1c7
fulles <ademon1998@yahoo.com>
2016-11-30 17:43:16 +0300

    UP

--- c/lab4/Variant51.c
+++ c/lab4/Variant51.c
@@ -4,35 +4,57 @@
 double acosh(double d);
 int main()
 {
-    double a, x, g, f, y, v, p;
-    int z, n, j, maxi, maxi2, mini, mini2, ar;
+    double a_from, x, g, f, y, v, p, a, a_to, ad, i;
+    int z, n, j, maxi, maxi2, mini, mini2, ar, tabul;
 
     printf("Хотите запустить программу? Нажмите 1 для продолжения, 2 для выхода \n");
     scanf("%d", &j);
     while ((j==1)) {
 
-        printf("Введите x и a \n");
+        printf("Введите x \n");
         scanf("%lf", &x);
-        scanf("%lf", &a);
+        printf("Введите a oт \n");
+        scanf("%lf", &a_from);
+        printf("Введите a до \n");
+        scanf("%lf", &a_to);
         printf("Введите количество шагов \n ");
         scanf("%d", &n);
         printf("Введите номер функции \n");
         scanf("%d", &z);
         p = x + (n * 0.1);
+        ad = (a_to-a_from)/n;
         double y_array[n];
+        double x_array[n];
         maxi, mini, maxi2, mini2, ar = 0;
+
 
         switch (z) {
             case 1:
                 while ((x < p)) {
                     x += 0.1;
+
                     if (((35 * pow(a, 2) + 37 * a * x + 6 * pow(x, 2)) > 0) ||
                         ((35 * pow(a, 2) + 37 * a * x + 6 * pow(x, 2)) < 0)) {
+                        for (i =a_from ; i < a_to; i+=ad)
+                            a = i;
                         g = (3 * (-3 * pow(a, 2) + 2 * a * x + 21 * pow(x, 2))) /
                             (35 * pow(a, 2) + 37 * a * x + 6 * pow(x, 2));
+                        x_array[ar] = x;
                         y_array[ar] = g;
                         ar += 1;
-                        printf("g= %f \n", g);
+                        printf("g= %lf \n", g);
+                        if (y_array[maxi] < y_array [maxi2]) {
+                            maxi = maxi2;
+                            maxi2 += 1;
+                        } else {
+                            maxi2 +=1;
+                        }
+                        if (y_array[mini] > y_array [mini2]) {
+                            mini = mini2;
+                            mini2 += 1;
+                        } else {
+                            mini2 +=1;
+                        }
 
                     } else {
                         printf("Нельзя делить на НОЛЬ \n");
@@ -43,27 +65,55 @@ int main()
             case 2:
                 while ((x < p)) {
                     x += 0.1;
+                    for (i =a_from ; i < a_to; i+=ad)
+                        a = i;
                     f = 1 / (cos(3 * pow(a, 2) + 5 * a * x + 2 * pow(x, 2)));
+                    x_array[ar] = x;
                     y_array[ar] = f;
                     ar += 1;
-                    printf("f= %f \n", f);
+                    printf("f= %lf \n", f);
+                    if (y_array[maxi] < y_array [maxi2]) {
+                        maxi = maxi2;
+                        maxi2 += 1;
+                    } else {
+                        maxi2 +=1;
+                    }
+                    if (y_array[mini] > y_array [mini2]) {
+                        mini = mini2;
+                        mini2 += 1;
+                    } else {
+                        mini2 +=1;
+                    }
                 }
             case 3:
 
                 v = (-12 * pow(a, 2) - 4 * a * x + pow(x, 2) + 1);
                 while ((x < p)) {
                     x += 0.1;
-
+                    for (i =a_from ; i < a_to; i+=ad)
+                        a = i;
                     if (v >= 1) {
 
                         y = acosh(-12 * pow(a, 2) - 4 * a * x + pow(x, 2) + 1);
+                        x_array[ar] = x;
                         y_array[ar] = y;
                         ar += 1;
-                        printf("f= %f \n", y);
-
-
+                        printf("f= %lf \n", y);
+                        if (y_array[maxi] < y_array [maxi2]) {
+                            maxi = maxi2;
+                            maxi2 += 1;
+                        } else {
+                            maxi2 +=1;
+                        }
+                        if (y_array[mini] > y_array [mini2]) {
+                            mini = mini2;
+                            mini2 += 1;
+                        } else {
+                            mini2 +=1;
+                        }
                     }
                     else {
+
                         printf("Невозможно извлечь Arccosh,пожалуйста,выберите другие переменные \n");
                     }
                 }
@@ -72,20 +122,16 @@ int main()
             default:
                 printf("Выберите номер функции 1,2 или 3");
         }
+        while (tabul < n ) {
+            printf("%lf\t",y_array[tabul]);
+            printf("X = %lf\n",x_array[tabul]);
+            tabul+=1;
 
-        printf("min = %lf \n", y_array[mini]);
+        }
+        printf("Наибольшее число массива = %lf \n", y_array[maxi]);
+        printf("Наименьшее число массива = %lf \n", y_array[mini]);
         printf("Хотите запустить программу? Нажмите 1 для продолжения, 2 для выхода \n");
         scanf("%d", &j);
     }
-}
 
-
-
-
-if (y_array[mini] > y_array[mini2]) {
-mini = mini2;
-mini2 += 1;
-}
-else{
-mini2 += 1;
 }
